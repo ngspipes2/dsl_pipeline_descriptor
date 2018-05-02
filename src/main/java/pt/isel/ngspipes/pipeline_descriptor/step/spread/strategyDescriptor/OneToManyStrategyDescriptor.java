6098102b1c0b8cdd6328ddf4db9897326a962718
BrunoMNDantas <bmndantas@gmail.com>
2018-05-02 15:25:31 +0100

    OneToManyStrategyDescriptor and OneToOneStrategyDescriptor with 2 constructors

--- src/main/java/pt/isel/ngspipes/pipeline_descriptor/step/spread/strategyDescriptor/OneToManyStrategyDescriptor.java
+++ src/main/java/pt/isel/ngspipes/pipeline_descriptor/step/spread/strategyDescriptor/OneToManyStrategyDescriptor.java
@@ -1,3 +1,11 @@
 package pt.isel.ngspipes.pipeline_descriptor.step.spread.strategyDescriptor;
 
-public class OneToManyStrategyDescriptor extends CombineStrategyDescriptor implements IOneToManyStrategyDescriptor { }
+public class OneToManyStrategyDescriptor extends CombineStrategyDescriptor implements IOneToManyStrategyDescriptor {
+
+    public OneToManyStrategyDescriptor(IStrategyDescriptor firstStrategy, IStrategyDescriptor secondStrategy) {
+        super(firstStrategy, secondStrategy);
+    }
+
+    public OneToManyStrategyDescriptor() { }
+
+}
--- src/main/java/pt/isel/ngspipes/pipeline_descriptor/step/spread/strategyDescriptor/OneToOneStrategyDescriptor.java
+++ src/main/java/pt/isel/ngspipes/pipeline_descriptor/step/spread/strategyDescriptor/OneToOneStrategyDescriptor.java
@@ -1,3 +1,11 @@
 package pt.isel.ngspipes.pipeline_descriptor.step.spread.strategyDescriptor;
 
-public class OneToOneStrategyDescriptor extends CombineStrategyDescriptor implements IOneToOneStrategyDescriptor { }
+public class OneToOneStrategyDescriptor extends CombineStrategyDescriptor implements IOneToOneStrategyDescriptor {
+
+    public OneToOneStrategyDescriptor(IStrategyDescriptor firstStrategy, IStrategyDescriptor secondStrategy) {
+        super(firstStrategy, secondStrategy);
+    }
+
+    public OneToOneStrategyDescriptor() { }
+
+}
